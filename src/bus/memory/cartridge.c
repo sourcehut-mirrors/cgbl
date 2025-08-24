4a7ee1841b11e1c56bfd37a4a3aea21830680ac5
David Jolly <jolly.a.david@gmail.com>
2025-08-24 11:32:15 -0700

    Revert "cartridge cleanup"
    This reverts commit ad06078487eebaa45f5df4b29c2b593a3dced29d.

--- src/bus/memory/cartridge.c
+++ src/bus/memory/cartridge.c
@@ -95,7 +95,7 @@ static const uint16_t ROM[] = {
 
 static struct {
     uint8_t hash;
-    char title[CGBL_CARTRIDGE_HEADER_TITLE_WIDTH];
+    char title[12];
     const cgbl_mapper_t *mapper;
     struct {
         uint32_t delay;
